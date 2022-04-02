e0d6a31a8eaa8dd0eb647a6fb919370763efc109
rcolejnr <ricardo.m.jnr@gmail.com>
2022-04-02 23:06:19 +0300

    options UI

--- app/src/main/java/com/rick/budgetly/feature_options/options/OptionsBody.kt
+++ app/src/main/java/com/rick/budgetly/feature_options/options/OptionsBody.kt
@@ -1,6 +1,7 @@
 package com.rick.budgetly.feature_options.options
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,14 +10,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Analytics
-import androidx.compose.material.icons.filled.Category
-import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -28,8 +27,13 @@ import androidx.navigation.NavHostController
 fun OptionsBody(viewModel: OptionsViewModel = hiltViewModel(), navController: NavHostController) {
 
     Scaffold(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.heightIn(100.dp, 350.dp), verticalArrangement = Arrangement.SpaceBetween) {
+        Column(verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             // am going to bed, love me, you.
+            AnimatedPieChart()
+
+            UserDetailsBox(name = "aldfjd", isActive = true, profilePic = Icons.Default.Person)
+
             OptionsRow(optionText = "Options", optionImage = Icons.Default.Settings, contentDescp = "options"){
 
             }
@@ -42,6 +46,43 @@ fun OptionsBody(viewModel: OptionsViewModel = hiltViewModel(), navController: Na
             OptionsRow(optionText = "Transactions", optionImage = Icons.Default.History, contentDescp = "transactions"){
 
             }
+        }
+    }
+}
+
+@Composable
+fun AnimatedPieChart() {
+    Box(
+        Modifier
+            .padding(vertical = 24.dp)
+            .size(255.dp)
+            .background(color = Color.White, shape = RoundedCornerShape(360.dp)))
+}
+
+@Composable
+fun UserDetailsBox(
+    name: String,
+    isActive: Boolean,
+    profilePic: ImageVector
+) {
+    Box(modifier = Modifier
+        .padding(vertical = 32.dp, horizontal = 8.dp)
+        .height(80.dp)
+        .border(2.dp, color = Color.DarkGray, shape = RoundedCornerShape(16.dp))
+    ){
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.wrapContentHeight(), verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.Start) {
+                Text(text = name, style = MaterialTheme.typography.body1, fontFamily = FontFamily.SansSerif)
+                Text(text = "Sync: $isActive", style = MaterialTheme.typography.body1, fontFamily = FontFamily.SansSerif)
+            }
+            Image(imageVector = profilePic, contentDescription = null, colorFilter = ColorFilter.tint(Color.White))
         }
     }
 }
@@ -65,13 +106,13 @@ fun OptionsRow(
             Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(4.dp)
-                .clickable { onClick() },
+                .clickable { onClick() }
+                .padding(12.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(text = optionText, style = MaterialTheme.typography.body1, fontFamily = FontFamily.Cursive)
-            Image(imageVector = optionImage, contentDescription = contentDescp)
+            Text(text = optionText, style = MaterialTheme.typography.body1, fontFamily = FontFamily.SansSerif)
+            Image(imageVector = optionImage, contentDescription = contentDescp, colorFilter = ColorFilter.tint(Color.White))
         }
     }
 }
