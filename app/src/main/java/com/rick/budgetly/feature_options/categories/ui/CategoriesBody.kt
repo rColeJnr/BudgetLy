070fba6186ed00c13bc17ca107d8ac840e42c528
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-04 23:21:59 +0300

    categories body, initial implementation

--- app/src/main/java/com/rick/budgetly/feature_options/categories/ui/CategoriesBody.kt
+++ app/src/main/java/com/rick/budgetly/feature_options/categories/ui/CategoriesBody.kt
@@ -2,22 +2,51 @@ package com.rick.budgetly.feature_options.categories.ui
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FoodBank
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.rick.budgetly.components.BaseBottomSheet
 import com.rick.budgetly.feature_options.categories.domain.Category
 import com.rick.budgetly.feature_options.categories.domain.CategoryImages
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CategoriesBody(navController: NavHostController) {
-    Text(text = "This is Categories screen")
+    
+    val scaffoldState = rememberScaffoldState()
+
+    val context = LocalContext.current
+
+    val modalBottomSheetState =
+        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val scope = rememberCoroutineScope()
+    
+    Scaffold(Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
+        
+        BaseBottomSheet(
+            state = modalBottomSheetState,
+            scope = scope,
+            navController = navController,
+            sheetContent = { /*TODO*/ }
+        ) {
+            LazyColumn{
+                items(listOf<Category>()){
+                    CategoriesRow(category = it)
+                }
+            }
+        }
+        
+    }
 }
 
 @Composable
