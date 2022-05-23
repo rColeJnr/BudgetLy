9d7a2f955d6693103d3dd16e9d1aff531db3a98a
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-24 01:59:16 +0300

    work with gradle scripts part 1

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,16 +1,31 @@
 object Modules {
 
     const val core = ":core"
+    const val app = ":app"
 
-//    const val core = ":options"
-//    const val core = ":core"
-//    const val core = ":core"
-//    const val core = ":core"
-//    const val core = ":core"
-//    const val core = ":core"
-//    const val core = ":core"
-//    const val core = ":core"
-//    const val core = ":core"
+    const val options_options = ":options:options"
+
+    const val options_overview = ":options:overview"
+
+    const val options_settings = ":options:settings"
+
+    const val options_categories = ":options:categories"
+    const val options_categories_data = ":options:categories:data-categories"
+    const val options_categories_screen = ":options:categories:screen-categories"
+
+    const val options_transaction = ":options:transactions"
+    const val options_transaction_data = ":options:transactions:data-transactions"
+    const val options_transaction_screen = ":options:transactions:screen-transactions"
+
+    const val bill_bills = ":bill:bills"
+    const val bill_data = ":bill:data"
+    const val bill_components = ":bill:components-bill"
+
+    const val account_accounts = ":account:accounts"
+    const val account_add_edit =  ":account:add-edit"
+    const val account_details = ":account:details"
+    const val account_data = ":account:data"
+    const val account_components = ":account:components"
 
 
 }
