11799d9a812caab675fcc64d6c76162caa4e5d73
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-24 00:31:07 +0300

    account name refactoring

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,16 @@
+object Modules {
+
+    const val core = ":core"
+
+//    const val core = ":options"
+//    const val core = ":core"
+//    const val core = ":core"
+//    const val core = ":core"
+//    const val core = ":core"
+//    const val core = ":core"
+//    const val core = ":core"
+//    const val core = ":core"
+//    const val core = ":core"
+
+
+}
