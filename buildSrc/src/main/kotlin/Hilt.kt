95be63553935957ad6706559973b1c082afbd97a
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-30 23:04:07 +0300

    downgraded kotlin version, want to successfully build project before updating gradle

--- buildSrc/src/main/kotlin/Hilt.kt
+++ buildSrc/src/main/kotlin/Hilt.kt
@@ -6,6 +6,6 @@ object Hilt {
     private const val hilt_viewmodel_version = "1.0.0-alpha03"
     const val hiltViewmodel = "androidx.hilt:hilt-lifecycle-viewmodel:$hilt_viewmodel_version"
     private const val hilt_compiler_version = "1.0.0"
-    const val hiltCompiler = "androidx.hilt:hilt-compiler:1.0.0"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:$hilt_compiler_version"
 
 }
